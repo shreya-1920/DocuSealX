de85bc32136328a6e5e06c9ed0ea5a220c5b92ae
shreya-1920 <shreyajain1902s@gmail.com>
2026-06-05 17:12:20 +0530

    Day 1 project setup complete

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("DocuSealX Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold">
+        DocuSealX
+      </h1>
+    </div>
+  )
+}
+
+export default App
