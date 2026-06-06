b0aff53976c44705d7dd29d7cb38f0d14a574160
shreya-1920 <shreyajain1902s@gmail.com>
2026-06-06 16:50:02 +0530

    completed day 2 tasks - Jwt authentication

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
-console.log(process.env.MONGO_URI);
+
 
 connectDB();
 
@@ -17,9 +19,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("DocuSealX Backend Running 🚀");
 });
-
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
